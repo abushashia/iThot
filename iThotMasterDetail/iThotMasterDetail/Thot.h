0c3f9945dcb814f8c171f4cb5dc3b08ecabd23bf
isaac.paquette <isaac.paquette@gmail.com>
2013-01-14 08:03:52 -0500

    Added Thot class

---
+++ iThotMasterDetail/iThotMasterDetail/Thot.h
@@ -0,0 +1,16 @@
+//
+//  Thot.h
+//  iThotMasterDetail
+//
+//  Created by Isaac Paquette on 1/14/13.
+//  Copyright (c) 2013 Adam Bushashia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Thot : NSObject
+
+@property (nonatomic) NSInteger itemId;
+@property (strong, nonatomic) NSString *text;
+
+@end
---
+++ iThotMasterDetail/iThotMasterDetail/Thot.m
@@ -0,0 +1,16 @@
+//
+//  Thot.m
+//  iThotMasterDetail
+//
+//  Created by Isaac Paquette on 1/14/13.
+//  Copyright (c) 2013 Adam Bushashia. All rights reserved.
+//
+
+#import "Thot.h"
+
+@implementation Thot
+
+@synthesize itemId;
+@synthesize text;
+
+@end
